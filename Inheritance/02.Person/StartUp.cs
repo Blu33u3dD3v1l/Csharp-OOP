696eeeced149461dea5daad507be4b50d1ca8225
Viktor Kachamakov <Kachamakov@abv.bg>
2022-07-01 14:12:56 +0300

    New Homework added.

---
+++ Inheritance/02.Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Person person = new Person(name, age);
+            if (person.Age >= 0 && child.Age > 0 &&  child.Age <= 15)
+            {
+                Console.WriteLine(child);
+            }
+          
+        }
+    }
+}
