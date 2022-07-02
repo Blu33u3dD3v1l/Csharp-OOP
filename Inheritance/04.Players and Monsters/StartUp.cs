5314d88426c5ff29b9eadc58bdbb0266102625fd
Viktor Kachamakov <Kachamakov@abv.bg>
2022-07-03 00:22:33 +0300

    My solution.

---
+++ Inheritance/04.Players and Monsters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Elf elf = new Elf("Ivan", 100);
+            Console.WriteLine(elf.ToString());
+            Knight knight = new Knight("Viktor", 10000);
+            Console.WriteLine(knight);
+        }
+    }
+}
