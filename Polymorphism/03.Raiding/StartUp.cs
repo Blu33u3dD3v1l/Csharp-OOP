6db3dd9340670cd8d4b0728e05328323579bff8a
Viktor <Kachamakov@abv.bg>
2022-07-13 22:23:50 +0300

    Solution added

---
+++ Polymorphism/03.Raiding/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());                      
+            var heroes = new List<BaseHero>();
+           
+
+            for (int i = 0; i < n; i++)
+            {
+                var playersName = Console.ReadLine();
+                var className = Console.ReadLine();
+                BaseHero hero = null;
+
+
+                if (className == "Paladin")
+                {
+                    hero = new Paladin(playersName);
+
+                }
+                else if (className == "Druid")
+                {
+                    hero = new Druid(playersName);                           
+                  
+
+                }
+                else if (className == "Rogue")
+                {
+                    hero = new Rogue(playersName);                   
+                   
+
+                }
+                else if (className == "Warrior")
+                {
+                    hero = new Warrior(playersName);                               
+                   
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                    i--;
+                    continue;
+                   
+                }
+                heroes.Add(hero);
+            
+
+            }
+
+
+
+            var num = int.Parse(Console.ReadLine());
+            var a = heroes.Sum(x => x.Power);
+            foreach (var item in heroes)
+            {
+                Console.WriteLine(item.CastAbility());
+            }
+            if(a >= num)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+
+        }
+    }
+}
